618ea9f64cf7c7b07ffcab2b575b9e9f09aff898
javalsai <jvssxxi@gmail.com>
2023-09-02 23:13:13 +0200

    feat: finish gpg import commands

--- src/commands/gpg.js
+++ src/commands/gpg.js
@@ -2,196 +2,299 @@
  * Discord Bot Gpg Command
  * @module arch
  */
-const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, Component, ComponentType } = require('discord.js');
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	ModalBuilder, ActionRowBuilder,
+	TextInputBuilder, TextInputStyle,
+	StringSelectMenuBuilder, StringSelectMenuOptionBuilder,
+	ComponentType,
+} = require('discord.js');
+const axios = require('axios').default;
+const openpgp = require('openpgp');
 const gpg = require('#modules/gpg.js');
 
 async function askForSignature(interaction) {
-    const modal = new ModalBuilder()
-        .setCustomId('verify-gpg')
-        .setTitle('Verify Identity');
+	const modal = new ModalBuilder()
+		.setCustomId('verify-gpg')
+		.setTitle('Verify Identity');
 
-    const signatureInput = new TextInputBuilder()
-        .setCustomId('signature')
-        .setLabel(`Sign: 'Yes, I'm ${interaction.user.id}'`)
-        .setStyle(TextInputStyle.Paragraph);
+	const signatureInput = new TextInputBuilder()
+		.setCustomId('signature')
+		.setLabel(`Clearsign: 'YES, I'M ${interaction.user.id.substring(0, 5)}'`)
+		.setStyle(TextInputStyle.Paragraph);
 
-    const actionRow = new ActionRowBuilder().addComponents(signatureInput);
+	const actionRow = new ActionRowBuilder().addComponents(signatureInput);
 
-    modal.addComponents(actionRow);
-    await interaction.showModal(modal);
-    const modalSubmit = await interaction.awaitModalSubmit({
-        time: 15 * 60 * 1000, // 15 minutes
-        filter: i => i.user.id === interaction.user.id,
-    }).catch(error => {
-        console.error(error);
-    });
-    if (modalSubmit) return modalSubmit;
+	modal.addComponents(actionRow);
+	await interaction.showModal(modal);
+	const modalSubmit = await interaction.awaitModalSubmit({
+		// 15 minutes
+		time: 15 * 60 * 1000,
+		filter: i => i.user.id === interaction.user.id,
+	}).catch(error => {
+		console.error(error);
+		return null;
+	});
+	if (modalSubmit) return modalSubmit;
+}
+
+async function askForSignatureAndVerify(interaction, key) {
+	// Ask for signature
+	const modalResponse = await askForSignature(interaction);
+	if (!modalResponse) return;
+
+	// Fetch key data
+	const keyData = (
+		await axios.get(key.dwUrl || key, { timeout: 2500 })
+			.catch(err =>
+				modalResponse.reply('Something went wrong fetching key: ' + err),
+			)
+	)?.data;
+	if (!keyData) return {};
+
+	// Parse key data
+	const publicKey = await openpgp.readKey({ armoredKey: keyData })
+		.catch(err =>
+			modalResponse.reply('Something went wrong parsing key: ' + err),
+		);
+	if (!publicKey) return {};
+
+	// Get and parse signature
+	const signature = modalResponse.fields.fields
+		.find(response => response.customId === 'signature').value;
+	const signedMessage =
+		await openpgp.readCleartextMessage({ cleartextMessage: signature })
+			.catch(err =>
+				modalResponse.reply('Error parsing clearsigned message: ' + err),
+			);
+	if (!signedMessage) return {};
+
+	// Verify signature
+	const verificationResult = await openpgp.verify({
+		message: signedMessage,
+		verificationKeys: publicKey,
+	});
+	const signatureResult = verificationResult.signatures[0];
+
+	try {
+		// Fails if invalid signature
+		await signatureResult.verified;
+
+		// Verify content
+		const expectedSentence = 'YES, I\'M ' + modalResponse.user.id.substring(0, 5);
+		if (signedMessage.text !== expectedSentence) {
+			modalResponse.reply('Signed message doesn\'t say "' + expectedSentence + '"');
+			return {};
+		}
+
+		// Made in the last 5 mins
+		const signDate = signedMessage.signature.packets[0].created;
+		if (Date.now() - signDate > 5 * 60 * 1000) {
+			modalResponse.reply('Signed more than 5 minutes ago, please clearsign again');
+			return {};
+		}
+	}
+	catch {
+		modalResponse.reply('Couldn\'t verify signature');
+		return {};
+	}
+
+	return {
+		signInteraction: modalResponse,
+		keyData: publicKey,
+		keyID: Buffer.from(publicKey.keyPacket.fingerprint)
+			.toString('hex').toUpperCase(),
+	};
 }
 
 function multipleKeysChoice(keys, id) {
-    function formatKeyToField(key, i) {
-        return {
-            name: `${i}. **${key.keyid}**\n<t:${key.date.getTime() / 1000}>`,
-            value: `[download](${key.dwUrl})\n` +
-                key.uids.map(([identity, date]) =>
-                    `${identity} <t:${date.getTime() / 1000}>`
-                ).join('\n'),
-            inline: false
-        };
-    }
-    function formatKeyToMenuOption(key, i) {
-        return new StringSelectMenuOptionBuilder()
-            .setLabel(`${i}. ${key.keyid}`)
-            .setDescription(`<t:${key.date.getTime() / 1000}>`)
-            .setValue(key.dwUrl);
-    }
+	function formatKeyToField(key, i) {
+		return {
+			name: `${i}. **${key.keyid}**\n<t:${key.date.getTime() / 1000}>`,
+			value: `[download](${key.dwUrl})\n` +
+				key.uids.map(([identity, date]) =>
+					`${identity} <t:${date.getTime() / 1000}>`,
+				).join('\n'),
+			inline: false,
+		};
+	}
+	function formatKeyToMenuOption(key, i) {
+		return new StringSelectMenuOptionBuilder()
+			.setLabel(`${i}. ${key.keyid}`)
+			.setDescription(`<t:${key.date.getTime() / 1000}>`)
+			.setValue(key.dwUrl);
+	}
 
-    const select = new StringSelectMenuBuilder()
-        .setCustomId(id)
-        .setPlaceholder('Choose a key!')
-        .addOptions(...keys.map(formatKeyToMenuOption));
+	const select = new StringSelectMenuBuilder()
+		.setCustomId(id)
+		.setPlaceholder('Choose a key!')
+		.addOptions(...keys.map(formatKeyToMenuOption));
 
-    const row = new ActionRowBuilder()
-        .addComponents(select);
+	const row = new ActionRowBuilder()
+		.addComponents(select);
 
-    const embed = new EmbedBuilder()
-        .setTitle('Select GPG key')
-        .setFields(...keys.map(formatKeyToField))
+	const embed = new EmbedBuilder()
+		.setTitle('Select GPG key')
+		.setFields(...keys.map(formatKeyToField));
 
 
-    return { embeds: [embed], components: [row] };
+	if (embed.length > 6000) return 'Found too many results, try a more specific query';
+	return { embeds: [embed], components: [row] };
 }
 
 function importKeyEmbed(importPromise, interaction) {
-    importPromise.then(keyData => {
-        interaction.reply('Sucessfully imported key!') // TODO: make nice and use keyData
-    }).catch(err => {
-        interaction.reply('Oops! something went wrong: ' + err)
-    })
+	importPromise.then(({ keyID, user }) => {
+		interaction.reply('Sucessfully imported key ' + keyID + ' to <@' + user.id + '>');
+	}).catch(err => {
+		interaction.reply('Oops! something went wrong: ' + err);
+	});
 }
 
+/* eslint-disable no-irregular-whitespace */
 // gpg
 // └── import
 //     ├── query
 //     │   └── string (required)
 //     └── file
 //         └── attachment (required)
+/* eslint-enable no-irregular-whitespace */
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('gpg')
-        .setDescription('Allows to link your gpg key to your discord account or verify messages')
-        .setDescriptionLocalizations({
-            'es-ES': 'Permite enlazar tu clave gpg a tu cuenta de discord o verificar mensajes',
-        })
-        .addSubcommandGroup(subCommandGroup => subCommandGroup
-            .setName('import')
-            .setNameLocalizations({
-                'es-ES': 'importar',
-            })
-            .setDescription('Imports a gpg key')
-            .setDescriptionLocalizations({
-                'es-ES': 'Importa una clave gpg',
-            })
-            .addSubcommand(subCommand => subCommand
-                .setName('query')
-                .setDescription('query')
-                .addStringOption(keyID => keyID
-                    .setName('query')
-                    .setDescription('Query to look for on keyservers')
-                    .setDescriptionLocalizations({
-                        'es-ES': 'Busqueda que haver en los servidores de claves',
-                    })
-                    .setRequired(true)
-                )
-            )
-            .addSubcommand(subCommand => subCommand
-                .setName('file')
-                .setNameLocalizations({
-                    'es-ES': 'archivo',
-                })
-                .setDescription('Public key attachment')
-                .addAttachmentOption(file => file
-                    .setName('file')
-                    .setNameLocalizations({
-                        'es-ES': 'archivo',
-                    })
-                    .setDescription('Public key')
-                    .setDescriptionLocalizations({
-                        'es-ES': 'Clave pública',
-                    })
-                    .setRequired(true)
-                )
-            )
-        ),
+	data: new SlashCommandBuilder()
+		.setName('gpg')
+		.setDescription('Allows to link your gpg key to your discord account or verify messages')
+		.setDescriptionLocalizations({
+			'es-ES': 'Permite enlazar tu clave gpg a tu cuenta de discord o verificar mensajes',
+		})
+		.addSubcommandGroup(subCommandGroup => subCommandGroup
+			.setName('import')
+			.setNameLocalizations({
+				'es-ES': 'importar',
+			})
+			.setDescription('Imports a gpg key')
+			.setDescriptionLocalizations({
+				'es-ES': 'Importa una clave gpg',
+			})
+			.addSubcommand(subCommand => subCommand
+				.setName('query')
+				.setDescription('query')
+				.addStringOption(keyID => keyID
+					.setName('query')
+					.setDescription('Query to look for on keyservers')
+					.setDescriptionLocalizations({
+						'es-ES': 'Busqueda que haver en los servidores de claves',
+					})
+					.setRequired(true),
+				),
+			)
+			.addSubcommand(subCommand => subCommand
+				.setName('file')
+				.setNameLocalizations({
+					'es-ES': 'archivo',
+				})
+				.setDescription('Public key attachment')
+				.addAttachmentOption(file => file
+					.setName('file')
+					.setNameLocalizations({
+						'es-ES': 'archivo',
+					})
+					.setDescription('Public key')
+					.setDescriptionLocalizations({
+						'es-ES': 'Clave pública',
+					})
+					.setRequired(true),
+				),
+			),
+		),
 
-    /**
-     * Execute the bot's command handling logic.
-     *
-     * @param {import('discord.js').Client} BOT - The Discord bot client
-     * @param {import('sqlite3').Database} DATABASE - SQLite3 database
-     * @param {import('discord.js').Interaction} interaction - A Discord interaction
-    */
-    async execute(BOT, DB, interaction) {
-        const gpgLib = gpg(BOT, DB);
+	/**
+	 * Execute the bot's command handling logic.
+	 *
+	 * @param {import('discord.js').Client} BOT - The Discord bot client
+	 * @param {import('sqlite3').Database} DATABASE - SQLite3 database
+	 * @param {import('discord.js').Interaction} interaction - A Discord interaction
+	*/
+	async execute(BOT, DB, interaction) {
+		const gpgLib = gpg(BOT, DB);
 
-        function importKey(key) {
-            gpgLib.importKeyToUser(key.dwUrl, interaction.user)
-                .then(() => {
-                    // !? make this a attractive embed if you want, key variable has plenty of information to put
-                    interaction.reply('Successfully imported ' + key.keyid);
-                })
-                .catch(interaction.reply);
-        }
+		function importKey(keyData, keyID) {
+			return gpgLib.importKeyToUser(keyData, keyID, interaction.user);
+		}
 
-        // interaction.user.id
-        switch (interaction.options._group) {
-            case 'import':
-                if (interaction.options._subcommand == 'query') {
-                    let query = interaction.options._hoistedOptions
-                        .find(option => option.name === 'query').value;
+		switch (interaction.options._group) {
+			case 'import':
+				// Query key on keyservers
+				if (interaction.options._subcommand == 'query') {
+					const query = interaction.options._hoistedOptions
+						.find(option => option.name === 'query').value;
 
-                    // Query keyservers for keys
-                    let results = await gpgLib.searchKeyservers(query)
-                        .catch(interaction.reply);
-                    if (!results) return; // Only triggers if search failed
+					// Query keyservers for keys
+					const results = await gpgLib.searchKeyservers(query)
+						.catch(err => interaction.reply(String(err)));
+					if (!results?.length) return;
 
-                    if (results.length == 0) {
-                        // No keys found
-                        interaction.reply('Counldn\'t find any result for the query');
-                    } else if (results.length == 1) {
-                        // One key found
-                        const modalResponse = await askForSignature(interaction);
-                        let signature = modalResponse.fields.fields.find(response => response.customId === 'signature').value;
-                        // TODO: Verify signature before importing it
-                        importKeyEmbed(importKey(results[0], interaction.user), modalResponse);
-                    } else if (results.length <= 25) {
-                        const reply = await interaction.reply(multipleKeysChoice(results, interaction.id));
-                        const collector = reply.createMessageComponentCollector({
-                            componentType: ComponentType.StringSelect,
-                            filter: i => i.user.id === interaction.user.id && i.customId === interaction.id,
-                            time: 60 * 1000 // 1min
-                        });
-                        collector.on('collect', interaction => {
-                            const keyToImport = results.find(key => key.dwUrl === interaction.values[0]);
-                            importKey(keyToImport);
-                        })
-                    } else {
-                        interaction.reply('Too many entries, try a more specific query.');
-                    }
-                }
-                else if (interaction.options._subcommand == 'file') {
-                    let attachment = interaction.options._hoistedOptions
-                        .find(option => option.name === 'file').attachment;
+					// No keys found
+					if (results.length == 0) {
+						interaction.reply('Couldn\'t find any result for the query');
+					}
+					// One key found
+					else if (results.length == 1) {
+						const { signInteraction, keyData, keyID } =
+							await askForSignatureAndVerify(interaction, results[0]);
+						if (!signInteraction) return;
 
-                    gpgLib.importKeyToUser(attachment.url, interaction.user)
-                        .catch(interaction.reply);
+						importKeyEmbed(
+							importKey(keyData, keyID),
+							signInteraction,
+						);
+					}
+					// Multiple keys found
+					else if (results.length <= 25) {
+						const replyToSend = multipleKeysChoice(results, interaction.id);
+						const reply = await interaction.reply(replyToSend);
+						if (typeof replyToSend === 'string') return;
 
-                    const modalResponse = await askForSignature(interaction);
-                    // TODO: Verify signature before importing
-                    importKeyEmbed(importKey(attachment, interaction.user), modalResponse);
-                }
-                break;
-        }
-    }
+						const collector = reply.createMessageComponentCollector({
+							componentType: ComponentType.StringSelect,
+							filter: i => i.user.id === interaction.user.id && i.customId === interaction.id,
+							// 1min
+							time: 60 * 1000,
+						});
+
+						collector.on('collect', async chosedInteraction => {
+							const keyToImport = results.find(key => key.dwUrl === chosedInteraction.values[0]);
+
+							const { signInteraction, keyData, keyID } =
+								await askForSignatureAndVerify(chosedInteraction, keyToImport);
+							if (!signInteraction) return;
+
+							importKeyEmbed(
+								importKey(keyData, keyID),
+								signInteraction,
+							);
+						});
+					}
+					// Way too many keys found
+					else {
+						interaction.reply('Too many entries, try a more specific query.');
+					}
+				}
+				// Import from attached file
+				else if (interaction.options._subcommand == 'file') {
+					const attachment = interaction.options._hoistedOptions
+						.find(option => option.name === 'file').attachment.url;
+
+					const { signInteraction, keyData, keyID } =
+						await askForSignatureAndVerify(interaction, attachment);
+					if (!signInteraction) return;
+
+					importKeyEmbed(
+						importKey(keyData, keyID),
+						signInteraction,
+					);
+				}
+				break;
+		}
+	},
 };
--- src/modules/gpg.js
+++ src/modules/gpg.js
@@ -10,70 +10,74 @@ const gpg = require('openpgp');
  */
 
 function genLookupUrl(prot, host, query) {
-    return `${prot}://${host}/pks/lookup?search=${query}&fingerprint=on&op=index`;
+	return `${prot}://${host}/pks/lookup?search=${query}&fingerprint=on&op=index`;
 }
 
 function listResuts(url, prot, host, fn) {
-    return new Promise((resolve, reject) => {
-        axios.get(url, { timeout: 2500 }).then(response => {
-            let results = parseResults(response.data, prot, host);
-            for (let result of results) fn(result);
-            resolve();
-        }).catch((err) => {
-            if(err.code === 'ECONNABORTED') return resolve(); // Just ignore if timed out
-            reject();
-        });
-    })
+	return new Promise((resolve, reject) => {
+		axios.get(url, { timeout: 2000 }).then(response => {
+			const results = parseResults(response.data, prot, host);
+			for (const result of results) fn(result);
+			resolve();
+		}).catch((err) => {
+			// Just ignore if timed out
+			if (err.code === 'ECONNABORTED') return resolve();
+			if (err.response?.status === 404) return reject('Couldn\'t find any result for the query');
+			reject(String(err));
+		});
+	});
 
 }
 
 // Please don't kill me, web scraping is hard
 function parseResults(htmlData = '', prot, host) {
-    let results =
-        htmlData.slice(htmlData.indexOf('<hr />'), htmlData.indexOf('</body>'))
-            .split('<hr />').slice(1);
+	let results =
+		htmlData.slice(htmlData.indexOf('<hr />'), htmlData.indexOf('</body>'))
+			.split('<hr />').slice(1);
 
-    if (results.length > 25) throw 'Too many entries, try a more specific query.';
+	if (results.length > 25) throw 'Too many entries, try a more specific query.';
 
-    results = results.map(result => {
-        let keyid, date, dwUrl, uids = [];
-        result = result.slice(5, -7).split('\n').filter(value => value)
-        result.forEach((line, i) => {
-            if (line.startsWith('<strong>pub')) {
-                let linkStart = line.indexOf('a');
+	results = results.map(result => {
+		let keyid, date, dwUrl;
+		const uids = [];
+		result = result.slice(5, -7).split('\n').filter(value => value);
+		result.forEach((line, i) => {
+			if (line.startsWith('<strong>pub')) {
+				const linkStart = line.indexOf('a');
 
-                keyid = line.slice(linkStart, line.indexOf('<', linkStart)).split('').reverse();
-                keyid = keyid.slice(0, keyid.indexOf('>')).reverse().join('');
-                keyid = keyid.split('/');
-                keyid[1] = keyid[1].toUpperCase();
-                keyid = keyid.join('/');
+				keyid = line.slice(linkStart, line.indexOf('<', linkStart)).split('').reverse();
+				keyid = keyid.slice(0, keyid.indexOf('>')).reverse().join('');
+				keyid = keyid.split('/');
+				keyid[1] = keyid[1].toUpperCase();
+				keyid = keyid.join('/');
 
-                dwUrl = line.slice(line.indexOf('"') + 1);
-                dwUrl = dwUrl.slice(0, dwUrl.indexOf('"'));
-                dwUrl = dwUrl.startsWith('/') ? prot + '://' + host + dwUrl : dwUrl;
+				dwUrl = line.slice(line.indexOf('"') + 1);
+				dwUrl = dwUrl.slice(0, dwUrl.indexOf('"'));
+				dwUrl = dwUrl.startsWith('/') ? prot + '://' + host + dwUrl : dwUrl;
 
-                date = line.split('').reverse();
-                date = date.slice(0, date.indexOf(' ')).reverse().join('');
-                date = new Date(date);
-            } else if (line.startsWith('<strong>uid')) {
-                let id, date;
+				date = line.split('').reverse();
+				date = date.slice(0, date.indexOf(' ')).reverse().join('');
+				date = new Date(date);
+			}
+			else if (line.startsWith('<strong>uid')) {
+				let id, uidDate;
 
-                let idStart = line.indexOf('uid">') + 5;
-                id = line.slice(idStart, line.indexOf('<', idStart));
-                id = id.replace('&lt;', '<').replace('&gt;', '>');
+				const idStart = line.indexOf('uid">') + 5;
+				id = line.slice(idStart, line.indexOf('<', idStart));
+				id = id.replace('&lt;', '<').replace('&gt;', '>');
 
-                let dateLine = result[i + 1];
-                let dateStart = dateLine.indexOf('/a> ') + 4;
-                date = dateLine.slice(dateStart, dateLine.indexOf(' ', dateStart));
-                date = new Date(date);
+				const dateLine = result[i + 1];
+				const dateStart = dateLine.indexOf('/a> ') + 4;
+				uidDate = dateLine.slice(dateStart, dateLine.indexOf(' ', dateStart));
+				uidDate = new Date(uidDate);
 
-                uids.push([id, date]);
-            }
-        });
-        return { keyid, date, dwUrl, uids }
-    })
+				uids.push([id, uidDate]);
+			}
+		});
+		return { keyid, date, dwUrl, uids };
+	});
 
-    return results;
+	return results;
 }
 
 
@@ -82,36 +86,34 @@ function parseResults(htmlData = '', prot, host) {
  * @param {import('sqlite3').Database} DB - SQLite3 database
  */
 function init(BOT, DB) {
-    return {
-        /**
-         * @param {string} query
-         * @return {Promise<Array.<QueryResult>>}
-         */
-        async searchKeyservers(query) {
-            const prot = 'http';
-            const keyservers = ['185.125.188.26'];
-            let results = [];
-            let promises = keyservers.map(keyserver =>
-                listResuts(
-                    genLookupUrl(prot, keyserver, query),
-                    prot, keyserver, item => results.push(item)
-                )
-            );
-            await Promise.all(promises).catch(err => {
-                throw err;
-            });
-            return results;
-        },
-        /**
-         * @param {string} url
-         * @param {import('discord.js').User} user
-         */
-        async importKeyToUser(url, user) {
-            const key = await axios.get(url);
-            console.log('Fetched ' + url + ' for user ' + user.displayName);
-            // !? Save key into user ID (user.id), throw string with message if any error
-        }
-    }
+	return {
+		/**
+		 * @param {string} query
+		 * @return {Promise<Array.<QueryResult>>}
+		 */
+		async searchKeyservers(query) {
+			const prot = 'http';
+			const keyservers = ['185.125.188.26'];
+			const results = [];
+			const promises = keyservers.map(keyserver =>
+				listResuts(
+					genLookupUrl(prot, keyserver, query),
+					prot, keyserver, item => results.push(item),
+				),
+			);
+			await Promise.all(promises);
+			return results;
+		},
+		/**
+		 * @param {string} url
+		 * @param {import('discord.js').User} user
+		 */
+		async importKeyToUser(keyData, keyID, user) {
+			console.log('We imported ' + keyID + ' to ' + user.displayName);
+			return { keyData, keyID, user };
+			// TODO: Save key into user ID (user.id), throw string with message if any error
+		},
+	};
 }
 
 module.exports = init;
